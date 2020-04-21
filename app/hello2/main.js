eed82f7d0a1e9568417f03c09537546529de9c9f
navapon.tantechasa <navapon.tantechasa@thomsonreuters.com>
2020-04-21 14:19:11 +0700

    complate all docker file and compose

--- app/hello2/main.js
+++ app/hello2/main.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/hello2', function (req, res) {
-	res.send('Hello-2')
+	res.send(`Hello-2 Container ID: ${process.env.HOSTNAME}`)
 })
 
 app.listen(8000, () => console.log('Hello-2 listening on port 8000!'))
